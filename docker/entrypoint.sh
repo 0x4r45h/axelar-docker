d37af9f02664d166c0915390129703a898b764a0
0x4r45h <0x4r45h@gmail.com>
2023-10-20 01:03:48 +0330

    wip 7

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#GENESIS_BINARY="/root/.axelar/cosmovisor/genesis/bin/lavad --home /root/.axelar"
 GENESIS_BINARY="axelard"
+
+update_snapshot() {
+  curl -L https://snapshots.kjnodes.com/axelar-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C /root/.axelar
+  [[ -f /root/.axelar/data/upgrade-info.json ]] && cp /root/.axelar/data/upgrade-info.json /root/.axelar/cosmovisor/genesis/upgrade-info.json
+}
 
 init_function() {
   if [ -z "$ACCOUNT_NAME" ]; then
@@ -32,19 +36,20 @@ init_function() {
     done
   fi
 
-  # Backup genesis.json
-  mv /root/.axelar/config/genesis.json /root/.axelar/config/genesis.json.bak && \
-  mv /root/.axelar/config/addrbook.json /root/.axelar/config/addrbook.json.bak
-  # Run lavad init command
-#  $GENESIS_BINARY init $MONIKER_NAME --chain-id $CHAIN_ID >/dev/null 2>&1
-  $GENESIS_BINARY init $MONIKER_NAME --chain-id $CHAIN_ID
+
+
+  # Run init command
+    $GENESIS_BINARY init $MONIKER_NAME --chain-id $CHAIN_ID >/dev/null 2>&1
   # Replace genesis.json with backed up file
-  mv /root/.axelar/config/genesis.json.bak /root/.axelar/config/genesis.json && \
-  mv /root/.axelar/config/addrbook.json.bak /root/.axelar/config/addrbook.json
-  # set chain id for testnet-2
-  #  $GENESIS_BINARY config chain-id lava-testnet-2
+    if [ "$CHAIN_ID" = "axelar-dojo-1" ]; then
+    cp -f /tmp/mainnet/genesis.json /root/.axelar/config/genesis.json &&
+    cp -f /tmp/mainnet/addrbook.json /root/.axelar/config/addrbook.json
+    else
+    cp -f /tmp/testnet/genesis.json /root/.axelar/config/genesis.json &&
+    cp -f /tmp/testnet/addrbook.json /root/.axelar/config/addrbook.json
+    fi
   # Set minimum gas price
-  sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.007uaxl\"|" /root/.axelar/config/app.toml
+  sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.007uaxl\"|" $HOME/.axelar/config/app.toml
 
   # Set pruning
   sed -i \
@@ -52,7 +57,8 @@ init_function() {
     -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
     -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
     -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
-    /root/.axelar/config/app.toml
+    $HOME/.axelar/config/app.toml
+
   # Print validator pubkey
 
   echo "Validator pubkey is : "
@@ -63,6 +69,9 @@ main() {
   case "$1" in
   "init")
     init_function
+    ;;
+  "update-snapshot")
+    update_snapshot
     ;;
   "start-node")
     cosmovisor start --p2p.seeds $SEED_NODE
